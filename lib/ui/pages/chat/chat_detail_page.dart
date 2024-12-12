b185d95499d8844ccb018ceffe35ad5bc4822518
LeeHJuu <xyident903@gmail.com>
2024-12-12 11:45:38 +0900

    feat: ChatListPage UI 제작

--- lib/ui/pages/chat/chat_detail_page.dart
+++ lib/ui/pages/chat/chat_detail_page.dart
@@ -1,21 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pet_4_ever/ui/pages/chat/widgets/chat_detail_list_view.dart';
-
-void main() {
-  runApp(const ProviderScope(child: MyApp()));
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: ChatDetailPage(),
-    );
-  }
-}
 
 final OWNER_NAME = "찡찡이 엄마";
 final SAMPLE_IMAGE_URL = "https://picsum.photos/200/300";
@@ -42,35 +26,35 @@ class ChatDetailPage extends StatelessWidget {
 
   Container chatInputSendBar() {
     return Container(
-          color: Colors.yellow[100],
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-          child: Row(
-            children: [
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration(
-                      isDense: true,
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      filled: true,
-                      fillColor: Colors.white),
-                ),
-              ),
-              IconButton(
-                onPressed: () {
-                  print("메세지 전송 버튼");
-                },
-                icon: Icon(
-                  Icons.pets,
-                  size: 30,
-                  color: Colors.brown,
-                ),
-              ),
-            ],
+      color: Colors.yellow[100],
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                  isDense: true,
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  filled: true,
+                  fillColor: Colors.white),
+            ),
           ),
-        );
+          IconButton(
+            onPressed: () {
+              print("메세지 전송 버튼");
+            },
+            icon: Icon(
+              Icons.pets,
+              size: 30,
+              color: Colors.brown,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   Container chatTitleBar() {
