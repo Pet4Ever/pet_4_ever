d570c5879d1cba95054e655475ad21e68634d6dd
UooUzzz <uuumonazzz@gmail.com>
2024-12-12 14:03:31 +0900

    add: fonts and images

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pet_4_ever/theme.dart';
 import 'package:pet_4_ever/ui/pages/home/home_page.dart';
+import 'package:pet_4_ever/ui/pages/login_join/login_page.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: ".env");
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: HomePage(),
+      home: LoginPage(),
     );
   }
 }
