19b86d7a55012276d297a504ea1c335bd3028b04
LeeHJuu <xyident903@gmail.com>
2024-12-12 20:24:37 +0900

    fix: selectedButton width 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,12 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:pet_4_ever/data/repository/friends_repository.dart';
-import 'package:pet_4_ever/firebase_options.dart';
 import 'package:pet_4_ever/theme.dart';
-import 'package:pet_4_ever/ui/pages/friends/friends_page.dart';
 import 'package:pet_4_ever/ui/pages/home/home_page.dart';
+import 'package:pet_4_ever/ui/pages/profile/upload_pet_profile.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
-  final friendsRepository = FriendsRepository();
-  await friendsRepository.getAll();
-
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
   runApp(ProviderScope(child: const MyApp()));
 }
 
@@ -25,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: FriendsPage(),
+      home: UploadPetProfile(),
     );
   }
 }
