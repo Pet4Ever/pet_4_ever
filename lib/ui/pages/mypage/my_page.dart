17c65295fd480e6c4f09021b985f4a1ff0697123
suhyunChoo <s_where@naver.com>
2024-12-13 19:26:32 +0900

    feat: MyPage 파이어베이스 연결

--- lib/ui/pages/mypage/my_page.dart
+++ lib/ui/pages/mypage/my_page.dart
@@ -1,26 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:pet_4_ever/data/repository/my_page_repository.dart';
 import 'package:pet_4_ever/ui/pages/mypage/widgets/dog_grid_view_box.dart';
 import 'package:pet_4_ever/ui/pages/mypage/widgets/user_profile_box.dart';
 
 class Mypage extends StatelessWidget {
-
-  //사용자가 등록한 강아지 프로필 수(임시 데이터)
-  int sampleCount = 3;
+  String owner_id = 'AdKG8Aq3FoTGSxWQGQNW';
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('우리집'),
-      ),
-      body: Column(
-        children: [
-          //사용자 프로필
-          UserProfileBox(),
-          //반려견 프로필 보여주는 그리드뷰 영역
-          DogGridViewBox(sampleCount),
-        ],
-      ),
-    );
+    return Consumer(builder: (context, ref, child) {
+      ref.read(myPageViewModel.notifier).getPetData(owner_id);
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('우리집'),
+        ),
+        body: Column(
+          children: [
+            //사용자 프로필
+            UserProfileBox(),
+            //반려견 프로필 보여주는 그리드뷰 영역
+            DogGridViewBox(),
+          ],
+        ),
+      );
+    });
   }
 }
