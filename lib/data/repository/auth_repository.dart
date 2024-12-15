a4f5d2ec9b262405cc65886f11192e124a27b0b6
UooUzzz <uuumonazzz@gmail.com>
2024-12-15 21:19:54 +0900

    fix: user -> auth_repository, user -> user_model.dart

---
+++ lib/data/repository/auth_repository.dart
@@ -0,0 +1,83 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+
+class AuthRepository {
+  final FirebaseAuth firebaseAuth;
+
+  AuthRepository(this.firebaseAuth);
+
+  // 회원가입
+  Future<void> Join(String email, String password, String name) async {
+    try {
+      final userCredential = await firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      // 새로만든 사용자 정보
+      User? user = userCredential.user;
+
+      if (user != null) {
+        // FireStore에 회원가입한 사용자 정보 저장
+        final colRef = FirebaseFirestore.instance.collection('user');
+        final docRef = colRef.doc(user.uid); // uid(유저고유값)으로 문서 생성
+
+        await docRef.set({
+          'name': name,
+          'email': email,
+          'uid': user.uid,
+        });
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  // 로그인
+  Future<User?> signIn(String email, String password) async {
+    try {
+      final userCredential = await firebaseAuth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential.user;
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  // 로그인 후, FireStore에서 사용자 정보 가져오기
+  Future<Map<String, dynamic>?> userData(String uid) async {
+    try {
+      // uid 고유번호로 사용자 정보 가져옴
+      final fromUid = await FirebaseFirestore.instance.collection('user').doc(uid).get();
+      if (fromUid.exists) {
+        return fromUid.data();
+      }
+      return null;
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+}
+
+
+
+// class UserRepository {
+//   final userCollection = FirebaseFirestore.instance.collection('user');
+
+//   Future<List<User>> getUsers() async {
+//     final querySnapshot = await userCollection.get();
+
+//     return querySnapshot.docs.map((doc) {
+//       return User.fromJson({
+//         'id': doc.id,
+//         ...doc.data() as Map<String, dynamic>,
+//       });
+//     }).toList();
+//   }
+// }
