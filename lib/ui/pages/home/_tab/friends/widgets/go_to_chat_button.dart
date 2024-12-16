02a76f1fc345386d7de642a7c5c27c30788cc9c6
LeeHJuu <xyident903@gmail.com>
2024-12-16 20:17:06 +0900

    add: 채팅 요청 버튼(임시)생성

---
+++ lib/ui/pages/home/_tab/friends/widgets/go_to_chat_button.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class GoToChatButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(onPressed: () {}, icon: Icon(Icons.ac_unit_outlined));
+  }
+}
