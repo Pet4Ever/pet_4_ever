028800b5730e366d564093f75159f79dcaf13c47
Yujin <yujin.mar.chung@gmail.com>
2024-12-17 10:45:40 +0900

    fix: firebase 중복호출 제거

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,14 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pet_4_ever/firebase_options.dart';
 import 'package:pet_4_ever/theme.dart';
+import 'package:pet_4_ever/ui/pages/home/_tab/friends/friends_page.dart';
+import 'package:pet_4_ever/ui/pages/home/home_page.dart';
 import 'package:pet_4_ever/ui/pages/login_join/login_page.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   await dotenv.load(fileName: ".env");
-  
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: LoginPage(),
+      home: HomePage(),
     );
   }
 }
